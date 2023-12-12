517d3342b1847bb81e864690609bc7027fd72afc
amazing79 <ignacioja@hotmail.com>
2023-12-12 20:44:49 -0300

    ajustes a codigo para cargar la pagina sin hacer download de la radio, hasta que de play al boton

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,12 +1,20 @@
 const PLAYING = 'playing';
 const PAUSED = 'paused';
-const STATUS = { PLAYING, PAUSED};
+const NOT_LOADED = 'not-loaded';
+const LOADED  = 'loaded';
+const RADIO_URL = 'https://radio.netyco.com:17238/;stream.mp3';
+const STATUS = { PLAYING, PAUSED, LOADED, NOT_LOADED};
 
 function handlePlayEvent(evt)
 {
     evt.target.classList.toggle('active');
     let player = document.getElementById('radioPlayer');
     let actualStatus = sessionStorage.getItem('status');
+    let loadedStatus = sessionStorage.getItem('loaded');
+    if(loadedStatus === STATUS.NOT_LOADED) {
+        sessionStorage.setItem('loaded', STATUS.LOADED);
+        player.src = RADIO_URL;
+    }
     if(actualStatus === STATUS.PAUSED) {
         player.play();
         actualStatus = STATUS.PLAYING;
@@ -20,6 +28,7 @@ function handlePlayEvent(evt)
 function init()
 {
     sessionStorage.setItem('status', STATUS.PAUSED) ;
+    sessionStorage.setItem('loaded', STATUS.NOT_LOADED);
     let radio = document.getElementById('radio');
     radio.addEventListener('click',  handlePlayEvent);
 }
